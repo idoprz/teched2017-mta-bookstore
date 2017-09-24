b04e88e64d5be97404d1b2abb3bbd1aad70e3ebe
P899395 <ran.hassid@gmail.com>
2017-09-24 06:52:14 +0000

    add resources

---
+++ ui/webapp/model/models.js
@@ -0,0 +1,48 @@
+sap.ui.define([
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/Device",
+	"book/ui/utils/utils"
+], function(JSONModel, Device, utils) {
+	"use strict";
+
+	return {
+
+		createDeviceModel: function() {
+			var oModel = new JSONModel(Device);
+			oModel.setDefaultBindingMode("OneWay");
+			return oModel;
+		},
+
+		createNewBookDialogEmptyModel: function() {
+			var oDialogModel = new JSONModel({
+				data: {
+					"bookId": parseInt(utils.generateRandomNumber(100, 9999)),
+					"bookName": "",
+					"isbn": "",
+					"price": undefined,
+					"priceCurrency": utils.getDefaultCurrencyCode(),
+					"authorName": ""
+				},
+				"canCreate": false
+			});
+
+			return oDialogModel;
+		},
+
+		createNewNookPlaceholdersModel: function() {
+			var oPlaceholdersModel = new JSONModel({
+				"bookNamePL": "Book Name",
+				"isbnPL": "ISBN",
+				"pricePL": "Price",
+				"authorNamePL": "Author Name"
+			});
+
+			// one way binding because placeholders cannot be changed from the UI
+			oPlaceholdersModel.setDefaultBindingMode(sap.ui.model.BindingMode.OneWay);
+
+			return oPlaceholdersModel;
+		}
+
+
+	};
+});
---
+++ ui/webapp/utils/utils.js
@@ -0,0 +1,31 @@
+jQuery.sap.require("jquery.sap.resources");
+sap.ui.define([], function() {
+	"use strict";
+
+	var Utils = {
+		
+		generateRandomNumber: function(min, max) {
+			return Math.random() * (max - min) + min;
+		},
+		
+		getDefaultCurrencyCode: function() {
+			return "EUR";
+		},
+		
+		showInfoMessage: function(sMessage) {
+			sap.m.MessageToast.show(sMessage, {
+				duration: 3000
+			});
+		},
+		
+		getDefaultResourceBundle: function() {
+			var oBundle = jQuery.sap.resources({
+				url: "i18n/i18n.properties",
+				locale: "en_GB"
+			});
+			return oBundle;			
+		}
+	};
+
+	return Utils;
+});
