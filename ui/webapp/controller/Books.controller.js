186ca4394baa234a3fd1cdf2b0c1d035eafc4afc
Ran Hassid <ran.hassid@gmail.com>
2017-09-24 17:18:34 +0300

    Revert "remove CUD resources"
    This reverts commit 45d2776a66a550964b8e99c969ab8f53e0f0c1ea.

--- ui/webapp/controller/Books.controller.js
+++ ui/webapp/controller/Books.controller.js
@@ -5,9 +5,72 @@ sap.ui.define([
 ], function(Controller, utils, models) {
 	"use strict";
 	return Controller.extend("book.ui.controller.Books", {
+		_addBookDialog: null,
+		/**
+		 *@memberOf book.ui.controller.Books
+		 */
+		onDeleteBook: function(oEvent) {
+			var oItemContext = oEvent.getSource().getBindingContext();
+			oItemContext.delete("$auto").then(function() {
+				utils.showInfoMessage(utils.getDefaultResourceBundle().getText("bookDeletedMessage"));
+			});
+		},
+		/**
+		 *@memberOf book.ui.controller.Books
+		 */
+		onAddBook: function() {
+			this._getNewBookDialog().open();
+		},
+		_getNewBookDialog: function() {
+			this._addBookDialog = sap.ui.xmlfragment("book.ui.view.NewBook", this);
+			// set empty JSON model 
+			this._addBookDialog.setModel(models.createNewBookDialogEmptyModel());
+			// set placeholders model
+			this._addBookDialog.setModel(models.createNewNookPlaceholdersModel(), 'PL');
+			this.getView().addDependent(this._addBookDialog);
+			return this._addBookDialog;
+		},
 		
+		onDialogCancel: function() {
+			this._addBookDialog.close();
+		},
+		
+		onDialogCreate: function() {
+			var oView = this.getView();
+			var self = this;
+			var oBinding = this.getView().byId("booksTable").getBinding("items");
+			var oDialogModel = this._getDialogModel();
 
-	
+			// make sure price is an integer field 			
+			oDialogModel.setProperty("/data/price", parseInt(oDialogModel.getProperty("/data/price")));
+			var oContext = oBinding.create(oDialogModel.oData.data);
+
+			oContext.created().then(function() {
+				// refresh binding in order to allow the creation of additional entities
+				self._addBookDialog.close();
+				utils.showInfoMessage(utils.getDefaultResourceBundle().getText("newBookCreatedInfoMessage"));
+			});
+
+			function resetBusy() {
+				self._addBookDialog.setBusy(false);
+			}
+			// lock UI 
+			this._addBookDialog.setBusy(true);
+			oView.getModel().submitBatch("myAppUpdateGroup").then(resetBusy, resetBusy);
+		},
+
+		onDialogInputChange: function() {
+			var oDialogModel = this._getDialogModel();
+			var canCreate = oDialogModel.getProperty("/data/bookName").length > 0 &&
+				oDialogModel.getProperty("/data/authorName").length > 0 &&
+				oDialogModel.getProperty("/data/isbn").length > 0 &&
+				oDialogModel.getProperty("/data/price").length > 0;
+			oDialogModel.setProperty("/canCreate", canCreate);
+		},
+
+		_getDialogModel: function() {
+			return this._addBookDialog.getModel();
+		}
 
 	});
 });
--- ui/webapp/model/models.js
+++ ui/webapp/model/models.js
@@ -11,6 +11,38 @@ sap.ui.define([
 			var oModel = new JSONModel(Device);
 			oModel.setDefaultBindingMode("OneWay");
 			return oModel;
+		},
+
+		createNewBookDialogEmptyModel: function() {
+			var oDialogModel = new JSONModel({
+				data: {
+					"bookId": parseInt(utils.generateRandomNumber(100, 9999)),
+					"bookName": "",
+					"isbn": "",
+					"price": undefined,
+					"priceCurrency": utils.getDefaultCurrencyCode(),
+					"authorName": ""
+				},
+				"canCreate": false
+			});
+
+			return oDialogModel;
+		},
+
+		createNewNookPlaceholdersModel: function() {
+			var oPlaceholdersModel = new JSONModel({
+				"bookNamePL": "Book Name",
+				"isbnPL": "ISBN",
+				"pricePL": "Price",
+				"authorNamePL": "Author Name"
+			});
+
+			// one way binding because placeholders cannot be changed from the UI
+			oPlaceholdersModel.setDefaultBindingMode(sap.ui.model.BindingMode.OneWay);
+
+			return oPlaceholdersModel;
 		}
+
+
 	};
 });
